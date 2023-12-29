32918d07e9a42ce70d7ed3b1e16332de3ff29370
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-12-29 16:58:06 -0400

    feat:add get users route

--- src/app/components/home/crud-service.service.ts
+++ src/app/components/home/crud-service.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CrudServiceService {
+export class CrudService {
 
-  constructor() { }
+  private readonly apiUrl = environment.apiUrl
+
+  constructor(private http:HttpClient) { }
+
+  getUsuarios():Observable<any>{
+    return this.http.get(`${this.apiUrl}/Usuario`)
+  }
 }
