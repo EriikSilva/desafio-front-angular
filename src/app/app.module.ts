0f2c223a19ff5c22008fee0cbbe410dc1168f552
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-12-29 11:18:27 -0400

    add:primeng modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,83 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NewUserDialogComponent } from './components/home/dialogs/new-user-dialog/new-user-dialog.component';
+
+import { DividerModule } from 'primeng/divider';
+import { TabViewModule } from 'primeng/tabview';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ToolbarModule } from 'primeng/toolbar';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
+import { ToastModule } from 'primeng/toast';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { SlideMenuModule } from 'primeng/slidemenu';
+import { InputMaskModule } from 'primeng/inputmask';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { TooltipModule } from 'primeng/tooltip';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { KeyFilterModule } from 'primeng/keyfilter';
+import { DropdownModule } from 'primeng/dropdown';
+import { CardModule } from 'primeng/card';
+import { SkeletonModule } from 'primeng/skeleton';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    NavbarComponent,
+    NewUserDialogComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
     AppRoutingModule,
-    ButtonModule
+
+    DividerModule,
+    TabViewModule,
+    ButtonModule,
+    InputTextModule,
+    PasswordModule,
+    ToolbarModule,
+    MessagesModule,
+    MessageModule,
+    ToastModule,
+    TableModule,
+    DialogModule,
+    SlideMenuModule,
+    InputMaskModule,
+    ConfirmPopupModule,
+    TooltipModule,
+    DropdownModule,
+    KeyFilterModule,
+    CardModule,
+    SkeletonModule,
+    ConfirmDialogModule,
+    ProgressSpinnerModule,
+    InputNumberModule,
+    RadioButtonModule,
+    MenubarModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
