c1586c700ed07c05f0c109c761c82d6563eec58b
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-12-30 20:43:42 -0400

    feat:register user

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,8 +1,10 @@
+import { CrudService } from './../home/crud-service.service';
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from './login.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ConfirmationService, MessageService } from 'primeng/api';
+import { PostUserDTO } from '../home/DTO/crudDTO';
 
 @Component({
   selector: 'app-login',
@@ -12,17 +14,78 @@ import { ConfirmationService, MessageService } from 'primeng/api';
 })
 export class LoginComponent implements OnInit{
 
-  constructor(private loginService:LoginService, private router: Router, private confirmationService: ConfirmationService, private messageService: MessageService){}
+  constructor(
+    private loginService:LoginService,
+    private router: Router,
+    private confirmationService: ConfirmationService,
+    private CrudService:CrudService,
+    private messageService: MessageService){}
+
+  nivel_acesso?:any;
+  slected_nivel_acesso:any;
 
   ngOnInit(): void {
       this.loginService.removeNome();
       this.loginService.removeNivelAcesso();
+
+      this.nivel_acesso = [
+        { 
+          id:1,
+          nvl_acesso: "Admin"
+        },
+        {
+          id:2,
+          nvl_acesso: "Usuario"
+        }
+      ]
   }
 
   formLogin = new FormGroup({
     email : new FormControl("", Validators.required),
     senha: new FormControl("", Validators.required)
   })
+
+  userFormDialog = new FormGroup({
+    nome: new FormControl('', Validators.required),
+    sobrenome: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    nivel_acesso: new FormControl('', Validators.required),
+    senha: new FormControl('', Validators.required)
+  })
+
+  saveUser(){
+    const formValue = this.userFormDialog.value;
+    const nome = String(formValue.nome);
+    const sobrenome = String(formValue.sobrenome);
+    const email = String(formValue.email);
+    const senha = String(formValue.senha)
+    const a = Object(formValue.nivel_acesso)
+    const { nvl_acesso } = a
+    
+    const body:PostUserDTO = {
+      nome, 
+      sobrenome,
+      email,
+      senha,
+      nivel_Acesso: nvl_acesso
+    }
+
+    this.CrudService.postUsuarios(body)
+    .subscribe({
+      next:(res:any) => {
+        const { mensagem } = res
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Sucesso ao cadastrar',
+          detail: mensagem
+        });
+
+        this.userFormDialog.reset();
+      }, error:(res:any) => {
+        console.log(res)
+      }
+    })    
+  }
 
   doLogin(){
     const formValue = this.formLogin.value;
